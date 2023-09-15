976eb7fe29822eb9b4b63e795c9b09e2c8492cd7
Fernando Lopez <fernandojalope343@gmail.com>
2023-09-15 19:24:39 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to TerminalPayments.web.ts
-// and on native platforms to TerminalPayments.ts
 import TerminalPaymentsModule from './TerminalPaymentsModule';
-import TerminalPaymentsView from './TerminalPaymentsView';
-import { ChangeEventPayload, TerminalPaymentsViewProps } from './TerminalPayments.types';
 
-// Get the native constant value.
-export const PI = TerminalPaymentsModule.PI;
-
-export function hello(): string {
-  return TerminalPaymentsModule.hello();
+export function getTheme(): string {
+  return TerminalPaymentsModule.getTheme();
 }
-
-export async function setValueAsync(value: string) {
-  return await TerminalPaymentsModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(TerminalPaymentsModule ?? NativeModulesProxy.TerminalPayments);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { TerminalPaymentsView, TerminalPaymentsViewProps, ChangeEventPayload };
